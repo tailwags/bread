bdac847454092b8cf52a4a15f09d2e4b6ef42a56
Matilde Morrone <contact@morrone.dev>
2024-12-22 18:37:10 +0100

    Use the new pflash firmware for qemu

--- scripts/qemu-x86_64.sh
+++ scripts/qemu-x86_64.sh
@@ -3,7 +3,7 @@
 qemu-system-x86_64 \
   -enable-kvm -machine q35 -device intel-iommu -cpu host -m 1G \
   -nic user,model=virtio-net-pci -smp 6 \
-  -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
-  -drive if=pflash,format=raw,file=OVMF_VARS.fd \
+  -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.4m.fd \
+  -drive if=pflash,format=raw,file=OVMF_VARS.4m.fd \
   -drive format=raw,file=fat:rw:esp \
   -device virtio-vga-gl -display gtk,gl=on
