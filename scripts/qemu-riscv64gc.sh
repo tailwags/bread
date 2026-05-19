ee41f208cd06b522ca3b3cd52de01673e4189089
Matilde Morrone <contact@matilde.pet>
2026-05-19 18:03:38 +0200

    Configure nix flake

--- scripts/qemu-riscv64gc.sh
+++ scripts/qemu-riscv64gc.sh
@@ -4,5 +4,5 @@ qemu-system-riscv64 \
   -machine virt,acpi=on,pflash0=pflash0 -smp 6 -cpu max -m 1G \
   -nic user,model=virtio-net-pci \
   -device virtio-gpu-gl -display gtk,gl=on -device qemu-xhci -device usb-kbd \
-  -blockdev node-name=pflash0,driver=file,read-only=on,filename=RISCV_VIRT_CODE.fd \
+  -blockdev node-name=pflash0,driver=file,read-only=on,filename="$RISCV_VIRT_CODE" \
   -device virtio-blk-device,drive=hd0 -drive format=raw,file=fat:rw:esp,id=hd0
